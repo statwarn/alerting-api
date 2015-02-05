ee8aba7b77ca555d2e823c80caf0bc3666a27049
Florent Chiron <florentchiron@gmail.com>
2015-02-05 14:39:45 +0100

    feat(ActionModel, ActionConfigurationModel): added models for action and action configuration

---
+++ app/models/ActionConfigurationModel.scala
@@ -0,0 +1,6 @@
+package models
+
+import play.api.libs.json.JsObject
+
+case class ActionConfigurationModel(configuration: JsObject) {
+}
---
+++ app/models/ActionModel.scala
@@ -0,0 +1,7 @@
+package models
+
+case class ActionModel(
+                        id: String,
+                        defaultConfiguration: ActionConfigurationModel
+                        ) {
+}
