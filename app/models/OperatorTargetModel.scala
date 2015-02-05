dd8a34633a71ff0776869bea81de6b77c14887da
Florent Chiron <florentchiron@gmail.com>
2015-02-05 18:37:02 +0100

    feat(operator, target): added OperatorTargetModel

---
+++ app/models/OperatorTargetModel.scala
@@ -0,0 +1,7 @@
+package models
+
+case class OperatorTargetModel(
+                                operator_id: String,
+                                target_id: String
+                                ) {
+}
