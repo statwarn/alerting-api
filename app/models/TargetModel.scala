714903587561dc77f217de681a9351d26063be07
Florent Chiron <florentchiron@gmail.com>
2015-03-02 15:12:03 +0100

    feat(TargetModel): added json Format

--- app/models/TargetModel.scala
+++ app/models/TargetModel.scala
@@ -2,6 +2,7 @@ package models
 
 import anorm.~
 import anorm.SqlParser.{bool, str}
+import play.api.libs.json.{Format, Json}
 
 case class TargetModel(
                         target_id: String,
@@ -10,6 +11,8 @@ case class TargetModel(
 }
 
 object TargetModel {
+  implicit val jsonFormat: Format[TargetModel] = Json.format[TargetModel]
+
   val simple = str("target_id") ~ bool("wildcard") map {
     case target_id ~ wildcard =>
       TargetModel(target_id, wildcard)
